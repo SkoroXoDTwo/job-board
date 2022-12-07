6fe2f892e9243bce4e2f0403397c17163fd911cf
Alexey Belkov <lexabelco2013@yandex.ru>
2022-12-07 23:50:17 +0500

    feat: создать компонент Header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <div className='app__container'>
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.scss'
+
+function Header() {
+  return (
+    <header className="header"></header>
+  );
+}
+
+export default Header;
