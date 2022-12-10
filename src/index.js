b86f89ad1442e3ca8b3f7fcbe0a7233f51d918e8
Alexey Belkov <lexabelco2013@yandex.ru>
2022-12-10 22:28:57 +0500

    feat: подключить и настроить react-persist

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import './index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/index';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+
   </Provider>
 );
